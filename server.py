810d95b20faaf1e4d8ea7ed2e8330f8271b7cdc0
Maria Triana <97359131+mtrian83@users.noreply.github.com>
2024-07-14 11:28:10 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from markupsafe import escape
+import os
 
 app = Flask(__name__)
 
@@ -10,4 +11,5 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
